80fae761977bb391ea9242e5c71a1ac827bf4fed
unknown <Sarvar@24N0002.ad.ums.uz>
2026-09-21 18:17:17 +0500

    Profilga bayroq qo'shildi

--- profil.js
+++ profil.js
@@ -34,9 +34,9 @@ window.updateProfileViewData = function() {
   if (cardFlag) cardFlag.src = `https://flagcdn.com/w20/${countryCode}.png`;
   if (cardCountryName) cardCountryName.textContent = countryName;
 
-  const countryInfoCard = document.getElementById("countryInfoCard");
-  const countryInfoFlag = document.getElementById("countryInfoFlag");
-  const countryInfoName = document.getElementById("countryInfoName");
+  const countryInfoCard = document.getElementById("profileCountry");
+  const countryInfoFlag = document.getElementById("profileCountryFlag");
+  const countryInfoName = document.getElementById("profileCountryName");
   if (countryName && countryInfoCard) {
     countryInfoCard.style.display = 'flex';
     if (countryInfoFlag) countryInfoFlag.src = `https://flagcdn.com/w160/${countryCode}.png`;
